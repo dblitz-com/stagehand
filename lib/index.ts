cb4f80b105496deaa48d4415f1fef79e0103252b
Miguel <36487034+miguelg719@users.noreply.github.com>
2025-07-09 15:50:07 -1000

    Update lib/index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -617,7 +617,7 @@ export class Stagehand {
       this.logger({
         category: "init",
         message:
-          "Custom LLM clients are currently not supported in API mode, share your feedback in slack! https://stagehand.dev/slack",
+          "Custom LLM clients are currently not supported in API mode",
         level: 1,
       });
       this.usingAPI = false;
